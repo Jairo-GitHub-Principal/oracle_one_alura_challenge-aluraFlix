84b967a5fc89f282b4151d699701688baf1bf75d
Jairo-GitHub-Principal <jairocesa@outlook.com.br>
2025-01-11 18:25:55 -0300

    correção no scroll da rolagem das categorias 'Horizontal

--- src/componentes/ReusableComponent/CategoriaMovie/categoriaMovieStyled.js
+++ src/componentes/ReusableComponent/CategoriaMovie/categoriaMovieStyled.js
@@ -31,11 +31,35 @@ const CategoriaMovieContainerStyled = styled.section` // vai  conter apenas  o c
     justify-content: flex-start;
     gap: 10px;
     width: 100%;
-    overflow-x: scroll;
+    overflow-x:auto;
    
     opacity: 0px;
     box-sizing: border-box;
     margin-top: 40px;
+
+    // personalizar o scrollbar 
+   
+     /* Personalização da barra de rolagem para Chrome, Edge e Safari */
+  &::-webkit-scrollbar {
+    height: 5px; /* Altura da barra de rolagem horizontal */
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: #888; /* Cor do controle da barra */
+    border-radius: 5px; /* Bordas arredondadas */
+  }
+
+  &::-webkit-scrollbar-thumb:hover {
+    background-color: #555; /* Cor ao passar o mouse */
+  }
+
+  &::-webkit-scrollbar-track {
+    background: #f1f1f1; /* Cor do fundo da barra */
+  }
+
+  /* Estilo para Firefox */
+  scrollbar-width: none; /* Reduz a largura no Firefox */
+  scrollbar-color: #888 #f1f1f1; /* Controle e fundo no Firefox */
         
    
 
